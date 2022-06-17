9856b9f31ce3cc17d00280fd82aab84b3dfde775
Dmitrii Ivanov <da_ivanov@list.ru>
2022-06-17 11:51:29 +0400

    1

---
+++ strings and lists/Lesson 2.10.3.py
@@ -0,0 +1,4 @@
+a = list(map(str, input().split()))
+b= "-".join(a[0]).upper()
+c= "-".join(a[1]).upper()
+print(b+ " "+ c)
---
+++ strings and lists/Lesson 2.10.5.py
@@ -0,0 +1,6 @@
+a = list(map(str, input().split()))
+i= "".join(a[0])
+o= "".join(a[1])
+f= "".join(a[2])
+
+print(f"""{f} {i[0]}.{o[0]}.""")
